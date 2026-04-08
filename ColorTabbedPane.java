e7e024de5eb63732593ecd8d4c1179abbaafbbc4
harshithakotian55-sketch <harshithakotian55@gmail.com>
2026-04-08 15:44:36 +0530

    Add files via upload

---
+++ ColorTabbedPane.java
@@ -0,0 +1,39 @@
+package Swings;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+public class ColorTabbedPane {
+	 public static void main(String[] args) {
+
+	        JFrame frame = new JFrame("Color Tabbed Pane");
+	        frame.setSize(400, 300);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        JTabbedPane tabbedPane = new JTabbedPane();
+
+	        JPanel cyanPanel = new JPanel();
+	        cyanPanel.setBackground(Color.CYAN);
+
+	        JPanel magentaPanel = new JPanel();
+	        magentaPanel.setBackground(Color.MAGENTA);
+
+	        JPanel yellowPanel = new JPanel();
+	        yellowPanel.setBackground(Color.YELLOW);
+
+	        tabbedPane.addTab("Cyan", cyanPanel);
+	        tabbedPane.addTab("Magenta", magentaPanel);
+	        tabbedPane.addTab("Yellow", yellowPanel);
+
+	        // Change Listener
+	        tabbedPane.addChangeListener(new ChangeListener() {
+	            public void stateChanged(ChangeEvent e) {
+	                int index = tabbedPane.getSelectedIndex();
+	                String title = tabbedPane.getTitleAt(index);
+	                System.out.println("Selected Color is: " + title);
+	            }
+	        });
+
+	        frame.add(tabbedPane);
+	        frame.setVisible(true);
+	    }
+	}
