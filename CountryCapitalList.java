5a32b66239665c8783b626b32cca2ee769b04627
harshithakotian55-sketch <harshithakotian55@gmail.com>
2026-04-18 14:42:23 +0530

    Add files via upload

---
+++ CountryCapitalList.java
@@ -0,0 +1,54 @@
+package Swings;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.util.HashMap;
+public class CountryCapitalList {
+	public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Country Capital List");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        // Countries Array
+        String countries[] = {
+                "USA", "India", "Vietnam", "Canada",
+                "Denmark", "France", "Great Britain",
+                "Japan", "Africa", "Greenland", "Singapore"
+        };
+
+        // Create JList
+        JList<String> countryList = new JList<>(countries);
+        countryList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        // Capital Mapping
+        HashMap<String, String> capitalMap = new HashMap<>();
+        capitalMap.put("USA", "Washington D.C.");
+        capitalMap.put("India", "New Delhi");
+        capitalMap.put("Vietnam", "Hanoi");
+        capitalMap.put("Canada", "Ottawa");
+        capitalMap.put("Denmark", "Copenhagen");
+        capitalMap.put("France", "Paris");
+        capitalMap.put("Great Britain", "London");
+        capitalMap.put("Japan", "Tokyo");
+        capitalMap.put("Africa", "Addis Ababa");
+        capitalMap.put("Greenland", "Nuuk");
+        capitalMap.put("Singapore", "Singapore");
+
+        // Add Listener
+        countryList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    String selectedCountry = countryList.getSelectedValue();
+                    String capital = capitalMap.get(selectedCountry);
+                    System.out.println("Capital of " + selectedCountry + " is: " + capital);
+                }
+            }
+        });
+
+        frame.add(new JScrollPane(countryList));
+        frame.setVisible(true);
+    }
+}
+
