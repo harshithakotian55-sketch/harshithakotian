3da4cb4056096ba0d373b023b655882187383213
harshithakotian55-sketch <harshithakotian55@gmail.com>
2026-04-20 22:13:38 +0530

    Update p6.java

--- p6.java
+++ p6.java
@@ -1,3 +1,4 @@
+//Write a Java program to iterate through all elements in a linked list starting at the specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))
 package lab1;
 
 import java.util.*;
